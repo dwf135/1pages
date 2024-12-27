17973db2005afeb13990c889c9810d71e70001aa
dwf135 <dwf135@qq.com>
2024-12-27 18:57:58 +0800

    Add files via upload

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
           {/* Logo */}
           <div className="flex-shrink-0 flex items-center">
             <Link to="/" className="text-xl font-bold text-gray-900 dark:text-white">
-              1Pages
+              时光机
             </Link>
           </div>
 
